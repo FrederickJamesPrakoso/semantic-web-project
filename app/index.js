502add8fda0c4bc3e7f0729cab3288967f6253fb
guntureka <guntureka1020@gmail.com>
2024-06-10 01:23:14 +0700

    add:data

--- app/index.js
+++ app/index.js
@@ -1,105 +1,98 @@
-const $rdf = require("rdflib");
-const fs = require("fs");
+// Mendapatkan elemen tombol dan input pencarian
+const searchButton = document.getElementById("searchButton");
+const searchInput = document.getElementById("searchInput");
+const limitInput = document.getElementById("limitInput");
 
-// const store = $rdf.graph();
+console.log(limitInput);
 
-// const data = fs.readFileSync("./app/data/rumah_sakit.ttl", "utf-8");
+// Menambahkan event listener ke tombol pencarian
+searchButton.addEventListener("click", function () {
+  // Mendapatkan nilai dari input pencarian
+  const searchValue = searchInput.value;
+  const limitValue = limitInput.value;
 
-// // const fetcher = new $rdf.Fetcher(store);
+  // Memanggil fungsi fetchData dengan nilai pencarian
+  fetchData(searchValue, limitValue);
+});
 
-// // const resourceUrl = `https://rumahsakit.com/ns/data#`;
-// // const resourceUrl = `./data/rumah_sakit.ttl`;
+async function fetchData(search, limit) {
+  const endpointUrl = "http://localhost:3030/rumahsakit/";
+  let sparqlQuery = "";
 
-// let doc = $rdf.sym("https://rumahsakit.com/ns/data#");
-// const mimeTyoe = "text/turtle";
-// $rdf.parse(data, store, doc.uri, mimeTyoe);
+  // Menentukan query berdasarkan apakah ada input pencarian atau tidak
+  if (search) {
+    sparqlQuery = `
+      PREFIX id: <https://rumahsakit.com/> 
+      PREFIX data: <https://rumahsakit.com/ns/data#> 
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 
-// async function fetchData() {
-//   const sparqlQuery = `
-//         SELECT * WHERE {
-//             ?datas
-//                         data:nama ?nama;
-//                         data:kabupaten ?kabupaten;
-//                         data:alamat ?alamat;
-//                         data:telepon ?telepon;
-//                         data:pemilik ?pemilik;
-//                         data:kelas ?kelas.
+      SELECT * WHERE {
+          ?datas			
+          data:kode ?kode;
+          data:nama ?nama;
+          data:kabupaten ?kabupaten;
+          data:alamat ?alamat;
+          data:telepon ?telepon;
+          data:pemilik ?pemilik;
+          data:kelas ?kelas.
+          FILTER 
+          (regex (?kode, '${search}', 'i') 
+          || regex (?nama, '${search}', 'i')
+          || regex (?kabupaten, '${search}', 'i') 
+          || regex (?alamat, '${search}', 'i')
+          || regex (?telepon, '${search}', 'i')
+          || regex (?pemilik, '${search}', 'i')
+          )
+      } LIMIT ${limit ? limit : "20"} `;
+  } else {
+    sparqlQuery = `
+      PREFIX id: <https://rumahsakit.com/> 
+      PREFIX data: <https://rumahsakit.com/ns/data#> 
+      PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> 
 
-//         } LIMIT 10
-//         `;
-
-//   const query = $rdf.SPARQLToQuery(sparqlQuery, false, store);
-
-//   try {
-//     store.query(query, (result) => {
-//       const data = Object.entries(result);
-//       console.log(data);
-//     });
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-
-fetchData();
-
-// function search() {
-//   const query = document.getElementById("searchInput").value;
-//   //   const url = `http://localhost:5000/search?q=${query}`;
-//   const url = `http://localhost:3030/rumahsakit`;
-
-//   fetch(url)
-//     .then((response) => response.json())
-//     .then((data) => {
-//       displayResults(data);
-//     })
-//     .catch((error) => {
-//       console.error("Error:", error);
-//     });
-// }
-
-// function displayResults(results) {
-//   const resultsBody = document.getElementById("resultsBody");
-//   resultsBody.innerHTML = "";
-
-//   let index = 1;
-
-//   console.log(results);
-
-//   results.forEach((result) => {
-//     const row = document.createElement("tr");
-//     row.innerHTML = `
-//             <td>${index++}</td>
-//             <td>${result.kode}</td>
-//             <td>${result.nama}</td>
-//             <td>${result.kabupaten}</td>
-
-//         `;
-//     resultsBody.appendChild(row);
-//   });
-// }
-
-// displayProducts(products);
-
-async function fetchData() {
-  const store = $rdf.graph();
-
-  const ID = $rdf.Namespace("https://rumahsakit.com");
-  const DATA = $rdf.Namespace("https://rumahsakit.com/ns/data#");
-
-  const turtleData = await fs.readFile("./app/data/rumah_sakit.ttl", "utf8");
-
-  $rdf.parse(turtleData, store, ID, "text/turtle");
-
-  const subjects = store.each(undefined, undefined, undefined);
-  let html = "";
-  for (let subject of subjects) {
-    const name = store.any(subject, DATA("name"));
-    const address = store.any(subject, DATA("address"));
-    html += `<h2>${name.value}</h2><p>${address.value}</p>`;
+      SELECT * WHERE {
+          ?datas			
+          data:kode ?kode;
+          data:nama ?nama;
+          data:kabupaten ?kabupaten;
+          data:alamat ?alamat;
+          data:telepon ?telepon;
+          data:pemilik ?pemilik;
+          data:kelas ?kelas.
+      } LIMIT ${limit ? limit : "20"} `;
   }
 
-  // Display the HTML
-  document.body.innerHTML = html;
+  // Melakukan fetch data dari endpoint SPARQL
+  fetch(
+    endpointUrl + "?query=" + encodeURIComponent(sparqlQuery) + "&format=json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      displayResults(data.results.bindings);
+    })
+    .catch((err) => console.error(err));
 }
 
+function displayResults(results) {
+  const resultsBody = document.getElementById("resultsBody");
+  resultsBody.innerHTML = "";
+
+  let index = 1;
+
+  results.forEach((result) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>${index++}</td>
+      <td>${result.kode.value}</td>
+      <td>${result.nama.value}</td>
+      <td>${result.kabupaten.value}</td>
+      <td>${result.alamat.value}</td>
+      <td>${result.pemilik.value}</td>
+      <td>${result.kelas.value}</td>
+    `;
+    resultsBody.appendChild(row);
+  });
+}
+
+// Memanggil fungsi fetchData saat halaman dimuat
 fetchData();
